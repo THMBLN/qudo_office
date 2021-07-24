4d9082893023afbb0f8dd8e81a9483a4cdaf6a50
THMBLN <72819355+THMBLN@users.noreply.github.com>
2021-07-24 21:42:20 +0200

    Add files via upload

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,6 @@ let currentPopup: any = undefined;
 const today = new Date();
 const time = today.getHours() + ":" + today.getMinutes();
 
-WA.room.onEnterZone('clock', () => {
-    currentPopup =  WA.ui.openPopup("clockPopup","It's " + time,[]);
-})
-
-WA.room.onLeaveZone('clock', closePopUp)
-
 function closePopUp(){
     if (currentPopup !== undefined) {
         currentPopup.close();
